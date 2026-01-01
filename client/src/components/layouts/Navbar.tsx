6420c3add92f4aea2ca495f7f59def89783bb870
Brijesh <softbrijeshio@gmail.com>
2026-01-01 18:12:40 +0530

    fix(client): fix navbar z-index

--- client/src/components/layouts/Navbar.tsx
+++ client/src/components/layouts/Navbar.tsx
@@ -19,7 +19,7 @@ function LinkItem({ to, label }: LinkItemProps) {
 
 export function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full p-3 bg-base-100 border-b border-secondary/10">
+    <nav className="fixed top-0 left-0 z-50 w-full p-3 bg-base-100 border-b border-secondary/10">
       <div className="w-full sm:w-[90%] flex items-center justify-between mx-auto">
         <Link
           to="/"
