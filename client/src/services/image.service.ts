14ef51c8abcdb9f8ddb9f72165e4208408b4bfb1
Brijesh <softbrijeshio@gmail.com>
2026-01-23 11:33:55 +0530

    refactor: migrate project to a pnpm monorepo architecture with TurboRepo.

--- client/src/services/image.service.ts
+++
@@ -1,6 +0,0 @@
-import { http } from "./http";
-
-export const ImageService = {
-  getAll: async () => (await http.get("/images")).data,
-  delete: async (id: string) => (await http.delete(`/images/${id}`)).data,
-};
--- client/src/types/secret.d.ts
+++
@@ -1,3 +0,0 @@
-export type SecretForm = {
-  name: string;
-};
